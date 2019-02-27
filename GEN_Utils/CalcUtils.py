a978b7e119995ae4d5ca022b667a55ca41bcd3f8
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2019-02-27 17:44:15 +1100

    Added sorting function to sensibly sort a list of strings or ints

--- GEN_Utils/CalcUtils.py
+++ GEN_Utils/CalcUtils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 import math
+import re
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from GEN_Utils.LoggerConfig import logger_config
@@ -191,3 +192,16 @@ def fit_plotter(fit_dictionary, x_label, y_label):
             plt.ylabel(y_label)
     plt.legend(loc='best')
     return fig
+
+
+
+def sorted_nicely( l ):
+    """ Sorts the given iterable in the way that is expected.
+
+    Required arguments:
+    l -- The iterable to be sorted.
+
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key = alphanum_key)
