4804363dd72544684472fcffb9fb814918b2fcb3
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2019-03-03 21:20:08 +1100

    Added dictionary utility for HDF5 store

--- GEN_Utils/HDF5_Utils.py
+++ GEN_Utils/HDF5_Utils.py
@@ -1,16 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def h5store(filename, df, **kwargs):
+def h5store(filename, key, df, **kwargs):
     store = pd.HDFStore(filename)
-    store.put('mydata', df)
-    store.get_storer('mydata').attrs.metadata = kwargs
+    store.put(key, df)
+    store.get_storer(key).attrs.metadata = kwargs
     store.close()
 
 def h5load(store):
     data = store['mydata']
     metadata = store.get_storer('mydata').attrs.metadata
     return data, metadata
+
+def dict_to_h5(filename, dictionary, **kwargs):
+    store = pd.HDFStore(filename)
+    for key, df in dictionary.items():
+        store.put(key, df)
+        store.get_storer(key).attrs.metadata = kwargs
+    store.close()
 
 # Example usage:
 # a = pd.DataFrame(data=pd.np.random.randint(0, 100, (10, 5)), columns=list('ABCED'))
